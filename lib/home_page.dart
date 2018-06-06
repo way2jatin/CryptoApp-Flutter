df4a5cfdc492c90ae3995e0530420da1bd8fbe04
Jatin Jha <jatin.jha@alluma.io>
2018-06-06 15:36:40 +0530

    Initial Commit for Crypto Flutter App

---
+++ lib/home_page.dart
@@ -0,0 +1,108 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:fluttercrypto/data/crypto_data.dart';
+import 'package:fluttercrypto/modules/crypto_presenter.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> implements CryptoListViewContract {
+  CryptoListPresenter _presenter;
+  List<Crypto> _currencies;
+  bool _isLoading;
+  final List<MaterialColor> _colors = [Colors.blue, Colors.indigo, Colors.red];
+
+  _HomePageState() {
+    _presenter = new CryptoListPresenter(this);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = true;
+    _presenter.loadCurrencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Crypto App"),
+          elevation: defaultTargetPlatform == TargetPlatform.iOS ? 0.0 : 5.0,
+        ),
+        body: _isLoading
+            ? new Center(
+                child: new CircularProgressIndicator(),
+              )
+            : _cryptoWidget());
+  }
+
+  Widget _cryptoWidget() {
+    return new Container(
+        child: new Column(
+      children: <Widget>[
+        new Flexible(
+          child: new ListView.builder(
+            itemCount: _currencies.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Crypto currency = _currencies[index];
+              final MaterialColor color = _colors[index % _colors.length];
+              return _getListItemUi(currency, color);
+            },
+          ),
+        )
+      ],
+    ));
+  }
+
+  ListTile _getListItemUi(Crypto currency, MaterialColor color) {
+    return new ListTile(
+      leading: new Image.network("http://cryptoicons.co/32@2x/color/"+currency.symbol.toLowerCase()+"@2x.png"),
+      title: new Text(currency.name,
+          style: new TextStyle(fontWeight: FontWeight.bold)),
+      subtitle:
+          _getSubtitleText(currency.price_usd, currency.percent_change_1h),
+      isThreeLine: true,
+    );
+  }
+
+  Widget _getSubtitleText(String priceUSD, String percentageChange) {
+    TextSpan priceTextWidget = new TextSpan(
+        text: "\$$priceUSD\n", style: new TextStyle(color: Colors.black));
+    String percentageChangeText = "1 hour: $percentageChange%";
+    TextSpan percentageChangeTextWidget;
+
+    if (double.parse(percentageChange) > 0) {
+      percentageChangeTextWidget = new TextSpan(
+          text: percentageChangeText,
+          style: new TextStyle(color: Colors.green));
+    } else {
+      percentageChangeTextWidget = new TextSpan(
+          text: percentageChangeText, style: new TextStyle(color: Colors.red));
+    }
+
+    return new RichText(
+        text: new TextSpan(
+            children: [priceTextWidget, percentageChangeTextWidget]));
+  }
+
+  @override
+  void onLoadCryptoComplete(List<Crypto> items) {
+    // TODO: implement onLoadCryptoComplete
+
+    setState(() {
+      _currencies = items;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void onLoadCryptoError() {
+    // TODO: implement onLoadCryptoError
+  }
+}
